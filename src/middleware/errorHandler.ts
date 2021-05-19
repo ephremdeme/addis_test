667dbbbb0131969327e760ca951f538550538120
ephremdeme <demelashephrem@gmail.com>
2021-05-20 02:25:54 +0300

    📦 NEW: adds error handler with some checkup

---
+++ src/middleware/errorHandler.ts
@@ -0,0 +1,36 @@
+import { NextFunction, Request, Response } from "express";
+import { ErrorResponse } from "../util/errorResponse";
+
+export const errorHandler =
+  () => (err: any, req: Request, res: Response, next: NextFunction) => {
+    let error = { ...err };
+    //Mongoose bad ObjectId
+
+    if (err.name === "CastError") {
+      const message = `Document not found with id of ${err.value}`;
+      error = new ErrorResponse(message, response_status_codes.not_found);
+    }
+    if (err.name)
+      res.status(error.statusCode || 500).json({
+        success: false,
+        error: {
+          message: error.message || err.message || "Server Error",
+          statusCode: error.statusCode || 500,
+        },
+        data: null,
+      });
+    else if (err.code)
+      res.status(error.statusCode || 500).json({
+        success: false,
+        error: { err },
+        data: null,
+      });
+  };
+
+export enum response_status_codes {
+  success = 200,
+  bad_request = 400,
+  internal_server_error = 500,
+  un_authorized = 401,
+  not_found = 404,
+}
