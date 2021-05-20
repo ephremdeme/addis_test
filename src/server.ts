fda4cf9b46eb58befd23650659317c9e503c14ce
ephremdeme <demelashephrem@gmail.com>
2021-05-20 18:01:25 +0300

    🐛 FIX: typo error

--- src/server.ts
+++ src/server.ts
@@ -9,7 +9,7 @@ if (!process.env.PORT) {
 
 const PORT: number = parseInt(process.env.PORT as string, 10) || 5001;
 
-app.use(errorHandler()); //I applied it last, cause i want it to run last and handle all the unhandled errors.
+app.use(errorHandler()); //I applied it last, coz i want it to run last and handle all the unhandled errors.
 
 app.listen(PORT, () => {
   console.log(" Express server listening on " + PORT);
